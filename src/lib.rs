a48dd431effb3f0cebf11f77276b1e6ea857ce51
xuxiaocheng <2762267080@qq.com>
2023-12-25 16:54:25 +0800

    feat(message): 支持加密发送消息

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 pub mod config;
 pub mod packet;
 pub mod starter;
+
+pub extern crate bytes;
+pub extern crate variable_len_reader;
+#[cfg(feature = "encrypt")]
+pub extern crate aes_gcm;
--- src/packet.rs
+++ src/packet.rs
@@ -1,5 +1,13 @@
 use std::io::Error;
-use bytes::BytesMut;
+#[cfg(feature = "encrypt")]
+use aes_gcm::{
+    AesGcm, Nonce,
+    aead::Aead,
+    aead::generic_array::typenum::U12,
+    aead::Error as AesGcmError,
+    aes::Aes256,
+};
+use bytes::{Bytes, BytesMut};
 use thiserror::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use variable_len_reader::asynchronous::{AsyncVariableReadable, AsyncVariableWritable};
@@ -11,6 +19,9 @@ pub enum PacketError {
     TooLarge(usize, usize),
     #[error("During io packet.")]
     IO(#[from] Error),
+    #[cfg(feature = "encrypt")]
+    #[error("During encrypting/decrypting bytes.")]
+    AES(#[from] AesGcmError)
 }
 
 #[inline]
@@ -19,7 +30,7 @@ fn check_bytes_len(len: usize) -> Result<(), PacketError> {
     if len > config { Err(PacketError::TooLarge(len, config)) } else { Ok(()) }
 }
 
-pub async fn write_packet<W: AsyncWriteExt + Unpin + Send>(stream: &mut W, bytes: &[u8]) -> Result<(), PacketError> {
+pub(crate) async fn write_packet<W: AsyncWriteExt + Unpin + Send>(stream: &mut W, bytes: &[u8]) -> Result<(), PacketError> {
     check_bytes_len(bytes.len())?;
     stream.write_u128_varint(bytes.len() as u128).await?;
     stream.write_more(&bytes).await?;
@@ -28,10 +39,33 @@ pub async fn write_packet<W: AsyncWriteExt + Unpin + Send>(stream: &mut W, bytes
     Ok(())
 }
 
-pub async fn read_packet<R: AsyncReadExt + Unpin + Send>(stream: &mut R) -> Result<BytesMut, PacketError> {
+pub(crate) async fn read_packet<R: AsyncReadExt + Unpin + Send>(stream: &mut R) -> Result<BytesMut, PacketError> {
     let len = stream.read_u128_varint().await? as usize;
     check_bytes_len(len)?;
     let mut buf = BytesMut::zeroed(len);
     stream.read_more(&mut buf).await?;
     Ok(buf)
+}
+
+pub async fn send<W: AsyncWriteExt + Unpin + Send>(stream: &mut W, bytes: &[u8]) -> Result<(), PacketError> {
+    write_packet(stream, bytes).await
+}
+
+pub async fn recv<R: AsyncReadExt + Unpin + Send>(stream: &mut R) -> Result<Bytes, PacketError> {
+    read_packet(stream).await.map(|m| m.into())
+}
+
+#[cfg(feature = "encrypt")]
+pub async fn send_with_encrypt<W: AsyncWriteExt + Unpin + Send>(stream: &mut W, message: &[u8], cipher: &(AesGcm<Aes256, U12>, Nonce<U12>)) -> Result<(), PacketError> {
+    let (cipher, nonce) = cipher;
+    let message = cipher.encrypt(nonce, message)?;
+    write_packet(stream, &message).await
+}
+
+#[cfg(feature = "encrypt")]
+pub async fn recv_with_encrypt<R: AsyncReadExt + Unpin + Send>(stream: &mut R, cipher: &(AesGcm<Aes256, U12>, Nonce<U12>)) -> Result<Bytes, PacketError> {
+    let (cipher, nonce) = cipher;
+    let message = read_packet(stream).await?;
+    let message = cipher.decrypt(nonce, &*message)?;
+    Ok(Bytes::from(message))
 }
