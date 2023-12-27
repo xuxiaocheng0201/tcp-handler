2de041a746eb384b93f7437fdfdebfc32183bc4f
xuxiaocheng <2762267080@qq.com>
2023-12-27 14:20:11 +0800

    feat(config): 优化config，添加doc

--- src/config.rs
+++ src/config.rs
@@ -1,42 +1,106 @@
+//! Global configuration for this crate.
+//!
+//! You may change the configuration by calling `set_config` function.
+//!
+//! # Example
+//! ```rust
+//! use tcp_handler::config::{Config, set_config};
+//!
+//! # fn main() {
+//! set_config(Config::default());
+//! # }
+//! ```
+
 use std::hint::spin_loop;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::RwLock;
 
+/// Global configuration.
+///
+/// # Example
+/// ```rust
+/// use tcp_handler::config::Config;
+///
+/// # fn main() {
+/// let config = Config::default();
+/// # let _ = config;
+/// # }
+/// ```
+#[derive(Debug, Clone, Copy)]
 pub struct Config {
+    /// `max_packet_size` is the maximum size of a packet in bytes.
+    /// It is used to limit the size of a packet that can be received or sent.
+    /// Default value is `1 << 20`.
+    ///
+    /// # Example
+    /// ```rust
+    /// use tcp_handler::config::Config;
+    ///
+    /// # fn main() {
+    /// let config = Config {
+    ///     max_packet_size: 1 << 10,
+    /// };
+    /// # let _ = config;
+    /// # }
+    /// ```
     pub max_packet_size: usize,
 }
 
-static mut CONFIG: Config = Config {
-    max_packet_size: 1 << 20,
-};
-
-static STATE: AtomicBool = AtomicBool::new(false);
-
-pub fn set_config(config: Config) {
-    loop {
-        if match STATE.compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst, ) { Ok(s) | Err(s) => s, } {
-            while STATE.load(Ordering::SeqCst) {
-                spin_loop();
-            }
-            continue
-        } else {
-            unsafe { CONFIG = config; }
-            STATE.store(false, Ordering::SeqCst);
-            break
-        };
-    };
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            max_packet_size: 1 << 20,
+        }
+    }
 }
 
+static CONFIG: RwLock<Config> = RwLock::new(Config {
+    max_packet_size: 1 << 20,
+});
+
+/// Sets the global configuration.
+///
+/// This function is recommended to only be called once during initialization.
+///
+/// # Example
+/// ```rust
+/// use tcp_handler::config::{Config, set_config};
+///
+/// # fn main() {
+/// set_config(Config::default());
+/// # }
+/// ```
+#[inline]
+pub fn set_config(config: Config) {
+    let mut c = CONFIG.write().unwrap();
+    *c = config;
+}
+
+/// Gets the global configuration.
+///
+/// # Example
+/// ```rust
+/// use tcp_handler::config::get_config;
+///
+/// # fn main() {
+/// let config = get_config();
+/// # let _ = config;
+/// # }
+/// ```
+#[inline]
+pub fn get_config() -> Config {
+    let mut c = CONFIG.read().unwrap();
+    *c.clone()
+}
+
+/// A cheaper shortcut of
+/// ```ignore
+/// get_config().max_packet_size
+/// ```
+#[inline]
 pub fn get_max_packet_size() -> usize {
-    loop {
-        if match STATE.compare_exchange(false, false, Ordering::SeqCst, Ordering::SeqCst, ) { Ok(s) | Err(s) => s, } {
-            while STATE.load(Ordering::SeqCst) {
-                spin_loop();
-            }
-            continue
-        } else {
-            return unsafe { CONFIG.max_packet_size }
-        };
-    };
+    let mut c = CONFIG.read().unwrap();
+    *c.max_packet_size
 }
 
 #[cfg(test)]
