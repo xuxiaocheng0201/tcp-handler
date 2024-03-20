b911348ea99984ce70859b93d280e067af5cf66f
xuxiaocheng <2762267080@qq.com>
2024-03-20 18:56:00 +0800

    feat(streams): 实现compress

--- src/streams/compress.rs
+++ src/streams/compress.rs
@@ -1,34 +1,34 @@
 use tokio::io::{AsyncRead, AsyncWrite};
 use crate::common::StarterError;
-use crate::raw::{self, send, recv};
+use crate::compress::{self, send, recv};
 use crate::streams::impl_tcp_handler;
 
-pub struct TcpServerHandlerRaw<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> {
+pub struct TcpServerHandlerCompress<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> {
     reader: R,
     writer: W,
     version: String,
 }
 
-impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> TcpServerHandlerRaw<R, W> {
+impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> TcpServerHandlerCompress<R, W> {
     pub async fn new<P: FnOnce(&str) -> bool>(mut reader: R, mut writer: W, identifier: &str, version_prediction: P, version: &str) -> Result<Self, StarterError> {
-        let init = raw::server_init(&mut reader, identifier, version_prediction).await;
-        let (_protocol_version, application_version) = raw::server_start(&mut writer, identifier, version, init).await?;
+        let init = compress::server_init(&mut reader, identifier, version_prediction).await;
+        let (_protocol_version, application_version) = compress::server_start(&mut writer, identifier, version, init).await?;
         Ok(Self { reader, writer, version: application_version })
     }
 }
 
-impl_tcp_handler!(server TcpServerHandlerRaw);
+impl_tcp_handler!(server TcpServerHandlerCompress);
 
 
-pub struct TcpClientHandlerRaw<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> {
+pub struct TcpClientHandlerCompress<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> {
     reader: R,
     writer: W,
 }
 
-impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> TcpClientHandlerRaw<R, W> {
+impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> TcpClientHandlerCompress<R, W> {
     async fn init(&mut self, identifier: &str, version: &str) -> Result<(), StarterError> {
-        let init = raw::client_init(&mut self.writer, identifier, version).await;
-        raw::client_start(&mut self.reader, init).await?;
+        let init = compress::client_init(&mut self.writer, identifier, version).await;
+        compress::client_start(&mut self.reader, init).await?;
         Ok(())
     }
 
@@ -39,7 +39,7 @@ impl<R: AsyncRead + Unpin, W: AsyncWrite + Unpin> TcpClientHandlerRaw<R, W> {
     }
 }
 
-impl_tcp_handler!(client TcpClientHandlerRaw);
+impl_tcp_handler!(client TcpClientHandlerCompress);
 
 
 #[cfg(test)]
@@ -48,21 +48,20 @@ mod tests {
     use bytes::{Buf, BufMut};
     use tokio::{spawn, try_join};
     use variable_len_reader::{VariableReader, VariableWriter};
-
-    use crate::streams::raw::{TcpClientHandlerRaw, TcpServerHandlerRaw};
+    use crate::streams::compress::{TcpClientHandlerCompress, TcpServerHandlerCompress};
     use crate::streams::tests::{check_send_recv, create};
 
     #[tokio::test(flavor = "multi_thread")]
     async fn connect() -> Result<()> {
         let (cr, cw, sr, sw) = create().await?;
-        let server = spawn(TcpServerHandlerRaw::new(sr, sw, "test", |v| v == "0", "0"));
-        let client = spawn(TcpClientHandlerRaw::new(cr, cw, "test", "0"));
+        let server = spawn(TcpServerHandlerCompress::new(sr, sw, "test", |v| v == "0", "0"));
+        let client = spawn(TcpClientHandlerCompress::new(cr, cw, "test", "0"));
         let (server, client) = try_join!(server, client)?;
         let (mut server, mut client) = (server?, client?);
 
         assert_eq!("0", server.get_client_version());
-        check_send_recv!(server, client, "Hello tcp-handler raw.");
-        check_send_recv!(client, server, "Hello tcp-handler raw.");
+        check_send_recv!(server, client, "Hello tcp-handler compress.");
+        check_send_recv!(client, server, "Hello tcp-handler compress.");
         Ok(())
     }
 }
--- src/streams/mod.rs
+++ src/streams/mod.rs
@@ -1,6 +1,7 @@
 //! Useful `TcpHandler`s
 
 pub mod raw;
+pub mod compress;
 
 use async_trait::async_trait;
 use bytes::{Buf, BytesMut};
@@ -9,14 +10,65 @@ use crate::common::PacketError;
 /// The basic handler trait, providing send and receive methods.
 #[async_trait]
 pub trait TcpHandler {
-    async fn send<B: Buf + Send>(&mut self, message: &mut B) -> Result<(), PacketError>;
-    async fn recv(&mut self) -> Result<BytesMut, PacketError>;
+    async fn handler_send<B: Buf + Send>(&mut self, message: &mut B) -> Result<(), PacketError>;
+    async fn handler_recv(&mut self) -> Result<BytesMut, PacketError>;
 
-    async fn send_recv<B: Buf + Send>(&mut self, message: &mut B) -> Result<BytesMut, PacketError> {
-        self.send(message).await?;
-        self.recv().await
+    async fn handler_send_recv<B: Buf + Send>(&mut self, message: &mut B) -> Result<BytesMut, PacketError> {
+        self.handler_send(message).await?;
+        self.handler_recv().await
     }
 }
+
+macro_rules! impl_tcp_handler {
+    (@ $struct: ident) => {
+        impl<R: ::tokio::io::AsyncRead + Unpin, W: ::tokio::io::AsyncWrite + Unpin> $struct<R, W> {
+            pub async fn send<B: ::bytes::Buf>(&mut self, message: &mut B) -> Result<(), $crate::protocols::common::PacketError> {
+                send(&mut self.writer, message).await
+            }
+
+            pub async fn recv(&mut self) -> Result<::bytes::BytesMut, $crate::protocols::common::PacketError> {
+                recv(&mut self.reader).await
+            }
+        }
+
+        #[::async_trait::async_trait]
+        impl<R: ::tokio::io::AsyncRead + Unpin + Send, W: ::tokio::io::AsyncWrite + Unpin + Send> $crate::streams::TcpHandler for $struct<R, W> {
+            async fn handler_send<B: ::bytes::Buf + Send>(&mut self, message: &mut B) -> Result<(), $crate::protocols::common::PacketError> {
+                self.send(message).await
+            }
+
+            async fn handler_recv(&mut self) -> Result<::bytes::BytesMut, $crate::protocols::common::PacketError> {
+                self.recv().await
+            }
+        }
+    };
+    (server $server: ident) => {
+        impl_tcp_handler!(@ $server);
+
+        impl<R: ::tokio::io::AsyncRead + Unpin, W: ::tokio::io::AsyncWrite + Unpin> $server<R, W> {
+            pub fn get_client_version(&self) -> &str {
+                &self.version
+            }
+        }
+    };
+    (client $client: ident) => {
+        impl_tcp_handler!(@ $client);
+
+        #[cfg(feature = "stream_net")]
+        impl $client<::tokio::io::BufReader<::tokio::net::tcp::OwnedReadHalf>, ::tokio::io::BufWriter<::tokio::net::tcp::OwnedWriteHalf>> {
+            #[cfg_attr(docsrs, doc(cfg(feature = "stream_net")))]
+            #[doc(concat!("Connection to `addr`, and construct the `", stringify!($client), "` using [", , stringify!($client), "::new]."))]
+            pub async fn connect<A: ::tokio::net::ToSocketAddrs>(addr: A, identifier: &str, version: &str) -> Result<Self, $crate::protocols::common::StarterError> {
+                let stream = ::tokio::net::TcpStream::connect(addr).await?;
+                let (reader, writer) = stream.into_split();
+                let reader = ::tokio::io::BufReader::new(reader);
+                let writer = ::tokio::io::BufWriter::new(writer);
+                Self::new(reader, writer, identifier, version).await
+            }
+        }
+    }
+}
+use impl_tcp_handler;
 
 #[cfg(test)]
 mod tests {
